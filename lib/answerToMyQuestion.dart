7de1f08e75b581e9650a9abbb697de8953ba845c
pratik <pratik17100@iiitnr.edu.in>
2019-06-21 14:14:29 +0530

    answers page almost completed

---
+++ lib/answerToMyQuestion.dart
@@ -0,0 +1,215 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:video_player/video_player.dart';
+import 'drawer.dart';
+import 'constants.dart';
+import 'dart:core';
+import 'chewieListNetwork.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'shared_preferences_helpers.dart';
+import 'package:teacher/shared_preferences_helpers.dart';
+
+class AnswersOfMyQuestion extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return AnswersOfMyQuestionState();
+  }
+}
+
+class AnswersOfMyQuestionState extends State<StatefulWidget> {
+  List list;
+  List<List<Map<String, dynamic>>> names = [];
+  String _name = USER_NAME;
+  String _email = EMAIL;
+
+  Future<void> setter() async {
+    var token = await getCurrentTokenId();
+    print(token);
+
+    var response = await http.get(
+        Uri.encodeFull(
+            "http://$serverIP:$serverPort/getAnswersToMyQuestions?tokenId=$token"),
+        headers: {"Accept": "application/json"});
+    if (response.statusCode == 200) {
+      List map = jsonDecode(response.body);
+      print(map);
+      for (var i = 0; i < map.length; i++) {
+        List<Map<String, dynamic>> curQueData = [];
+        curQueData.add({});
+        for (var j = 1; j < map[i].length; j++) {
+          var response = await http.get(
+              "http://$serverIP:$serverPort/getUserFromAnswer?ansName=" +
+                  map[i][j]
+                      .toString()
+                      .substring(map[i][j].toString().lastIndexOf("/") + 1));
+          var resJson = jsonDecode(response.body);
+          curQueData.add(resJson);
+        }
+        names.add(curQueData);
+      }
+      // print(map);
+      print(names);
+      setState(() {
+        list = map;
+        // print(list.toString());
+      });
+    } else {
+      Fluttertoast.showToast(
+        msg: 'Unable to fetch your answers',
+        toastLength: Toast.LENGTH_SHORT,
+      );
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setter();
+  }
+
+  showQuestion(String quesPath) async {
+    String fileName = quesPath.substring(quesPath.lastIndexOf("/") + 1);
+    print("http://$serverIP:$serverPort/downloadFile/$fileName");
+    VideoPlayerController quesController = VideoPlayerController.network("http://$serverIP:$serverPort/downloadFile/$fileName");
+    Future<void> quesInit = quesController.initialize();
+    print("initialised");
+    // TODO future builder video player is not working in dialog
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: FutureBuilder(
+                future: quesInit,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    // If the VideoPlayerController has finished initialization, use
+                    // the data it provides to limit the aspect ratio of the VideoPlayer.
+                    return AspectRatio(
+                      aspectRatio: 16 / 9,
+                      // Use the VideoPlayer widget to display the video.
+                      child: VideoPlayer(quesController),
+                    );
+                  } else {
+                    print("connection state is not connected");
+                    // If the VideoPlayerController is still initializing, show a
+                    // loading spinner.
+                    return Center(child: CircularProgressIndicator());
+                  }
+                },
+              ),
+          );
+        });
+  }
+
+  List<Widget> getVideos() {
+    List<Widget> listArray = List<Widget>();
+    if (list != null) {
+      if (list.length != 0) {
+        for (int i = 0; i < list.length; i++) {
+          // if (list[i].length == 1) {
+          //   listArray.add(Padding(
+          //       padding: EdgeInsets.all(15.0),
+          //       child: Card(
+          //           child: Text(
+          //               "q $i No Answer Available for this question yet"))));
+          // } else {
+          List<Widget> answers = [];
+          answers.add(ChewieListItemNet(
+              url:
+                  "http://$serverIP:$serverPort/downloadFile/${list[i][0].toString().substring(list[i][0].toString().lastIndexOf("/") + 1)}",
+              key: UniqueKey()));
+          for (int j = 1; j < list[i].length; j++) {
+            print("$i $j");
+            Widget nameWidget = Text((names[i])[j]["Name"]);
+            VideoPlayerController _controller = VideoPlayerController.network(
+                "http://$serverIP:$serverPort/downloadAnswer/" +
+                    list[i][j]
+                        .substring(list[i][j].toString().lastIndexOf("/") + 1));
+            Future<void> _initializeVideoPlayerFuture =
+                _controller.initialize().then((value){_controller.play();});
+
+            Widget vidPlayer = FutureBuilder(
+              future: _initializeVideoPlayerFuture,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  // If the VideoPlayerController has finished initialization, use
+                  // the data it provides to limit the aspect ratio of the VideoPlayer.
+                  return AspectRatio(
+                    aspectRatio: 16 / 9,
+                    // Use the VideoPlayer widget to display the video.
+                    child: VideoPlayer(_controller),
+                  );
+                } else {
+                  // If the VideoPlayerController is still initializing, show a
+                  // loading spinner.
+                  return Center(child: CircularProgressIndicator());
+                }
+              },
+            );
+            // Widget vPlayer = new ChewieListItemNet(
+            //   url: "http://$serverIP:$serverPort/downloadAnswer/" +
+            //       list[i][j]
+            //           .toString()
+            //           .substring(list[i][j].toString().lastIndexOf("/") + 1),
+            //   key: UniqueKey(),
+            // );
+            answers.add(Column(
+              children: <Widget>[nameWidget, vidPlayer],
+            ));
+          }
+          Widget toAdd = Padding(
+              padding: EdgeInsets.all(15.0),
+              child: Card(
+                //color: Color.fromARGB(255, 76, 175, 80),
+                elevation: 5.0,
+                child: Column(
+                  children: <Widget>[
+                    GestureDetector(
+                      child: Text(
+                        "Question $i",
+                        style: TextStyle(
+                            color: Colors.blue,
+                            fontStyle: FontStyle.italic,
+                            decoration: TextDecoration.underline),
+                      ),
+                      onTap: () async {
+                        await showQuestion(list[i][0]);
+                      },
+                    ),
+                    Column(
+                      children: answers,
+                    ),
+                  ],
+                ),
+              ));
+          listArray.add(toAdd);
+        }
+      } else {
+        listArray.add(
+            Center(child: Text("Sorry, No answers to your questions yet!")));
+      }
+    } else {
+      listArray
+          .add(Center(child: Text("Sorry, No answers to your questions yet!")));
+    }
+    return listArray;
+  }
+
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        drawer: NavDrawer(
+          email: EMAIL,
+          userName: USER_NAME,
+        ),
+        appBar: new AppBar(
+          title: new Text("Video Question App"),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: getVideos(),
+          ),
+        ));
+  }
+}
---
+++ lib/answer_video.dart
@@ -0,0 +1,374 @@
+import 'dart:io';
+
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:http_parser/http_parser.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:teacher/shared_preferences_helpers.dart';
+import 'package:http/http.dart' as http;
+import 'constants.dart';
+
+class AnswerVideoRecorder extends StatefulWidget {
+  final String questionName;
+  AnswerVideoRecorder(this.questionName);
+  @override
+  _AnswerVideoRecorderState createState() {
+    return _AnswerVideoRecorderState();
+  }
+}
+
+class _AnswerVideoRecorderState extends State<AnswerVideoRecorder> {
+  CameraController controller;
+  String videoPath;
+  List<CameraDescription> cameras;
+  int selectedCameraIdx = 0;
+  bool toUpload = true;
+
+  String currentTime;
+  String email;
+  String videoDirectory;
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  Future<void> _setUpCameras() async {
+    try {
+      // initialize cameras.
+      cameras = await availableCameras();
+      // initialize camera controllers.
+      controller = new CameraController(cameras[0], ResolutionPreset.medium);
+      await controller.initialize();
+    } on CameraException catch (e) {
+      print(e);
+      await controller.initialize();
+    }
+    if (mounted == false) return;
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    _setUpCameras();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: AppBar(
+        title: const Text('Ask Question'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              child: Padding(
+                padding: const EdgeInsets.all(1.0),
+                child: Center(
+                  child: _cameraPreviewWidget(),
+                ),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.black,
+                border: Border.all(
+                  color: controller != null && controller.value.isRecordingVideo
+                      ? Colors.redAccent
+                      : Colors.grey,
+                  width: 3.0,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                _cameraTogglesRowWidget(),
+                _captureControlRowWidget(),
+                Expanded(
+                  child: SizedBox(),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _cameraPreviewWidget() {
+    print("controller: $controller");
+    print(controller?.value);
+    if (controller == null || !controller.value.isInitialized) {
+      return const Text(
+        'Loading',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 20.0,
+          fontWeight: FontWeight.w900,
+        ),
+      );
+    }
+
+    return AspectRatio(
+      aspectRatio: controller.value.aspectRatio,
+      child: CameraPreview(controller),
+    );
+  }
+
+  Widget _cameraTogglesRowWidget() {
+    if (cameras == null) {
+      print("cameras is null");
+      return Row();
+    }
+
+    CameraDescription selectedCamera = cameras[selectedCameraIdx];
+    CameraLensDirection lensDirection = selectedCamera.lensDirection;
+
+    return Expanded(
+      child: Align(
+        alignment: Alignment.centerLeft,
+        child: FlatButton.icon(
+            onPressed: _onSwitchCamera,
+            icon: Icon(_getCameraLensIcon(lensDirection)),
+            label: Text(
+                "${lensDirection.toString().substring(lensDirection.toString().indexOf('.') + 1)}")),
+      ),
+    );
+  }
+
+  IconData _getCameraLensIcon(CameraLensDirection direction) {
+    switch (direction) {
+      case CameraLensDirection.back:
+        return Icons.camera_rear;
+      case CameraLensDirection.front:
+        return Icons.camera_front;
+      case CameraLensDirection.external:
+        return Icons.camera;
+      default:
+        return Icons.device_unknown;
+    }
+  }
+
+  /// Display the control bar with buttons to record videos.
+  Widget _captureControlRowWidget() {
+    return Expanded(
+      child: Align(
+        alignment: Alignment.center,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.videocam),
+              color: Colors.blue,
+              onPressed: controller != null &&
+                      controller.value.isInitialized &&
+                      !controller.value.isRecordingVideo
+                  ? _onRecordButtonPressed
+                  : null,
+            ),
+            IconButton(
+              icon: const Icon(Icons.stop),
+              color: Colors.red,
+              onPressed: controller != null &&
+                      controller.value.isInitialized &&
+                      controller.value.isRecordingVideo
+                  ? _onStopButtonPressed
+                  : null,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _onSwitchCamera() {
+    selectedCameraIdx =
+        selectedCameraIdx < cameras.length - 1 ? selectedCameraIdx + 1 : 0;
+    CameraDescription selectedCamera = cameras[selectedCameraIdx];
+
+    _onCameraSwitched(selectedCamera);
+
+    setState(() {
+      selectedCameraIdx = selectedCameraIdx;
+    });
+  }
+
+  Future<void> _onCameraSwitched(CameraDescription cameraDescription) async {
+    if (controller != null) {
+      await controller.dispose();
+    }
+
+    controller = CameraController(cameraDescription, ResolutionPreset.high);
+
+    // If the controller is updated then update the UI.
+    controller.addListener(() {
+      if (mounted) {
+        setState(() {});
+      }
+
+      if (controller.value.hasError) {
+        Fluttertoast.showToast(
+            msg: 'Camera error ${controller.value.errorDescription}',
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.CENTER,
+            timeInSecForIos: 1,
+            backgroundColor: Colors.red,
+            textColor: Colors.white);
+      }
+    });
+
+    try {
+      print("initializing");
+      await controller.initialize();
+      print("intialze complete");
+    } on CameraException catch (e) {
+      _showCameraException(e);
+    }
+
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+
+
+  void _onRecordButtonPressed() {
+    _startVideoRecording().then((String filePath) {
+      if (filePath != null) {
+        Fluttertoast.showToast(
+            msg: 'Recording video started',
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.CENTER,
+            timeInSecForIos: 1,
+            backgroundColor: Colors.grey,
+            textColor: Colors.white
+        );
+      }
+    });
+  }
+
+  Future<String> _startVideoRecording() async {
+    email = await getFromSP(EMAIL_KEY_SP);
+    if (!controller.value.isInitialized) {
+      Fluttertoast.showToast(
+          msg: 'Please wait',
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.CENTER,
+          timeInSecForIos: 1,
+          backgroundColor: Colors.grey,
+          textColor: Colors.white);
+
+      return null;
+    }
+
+    // Do nothing if a recording is on progress
+    if (controller.value.isRecordingVideo) {
+      return null;
+    }
+
+    final Directory appDirectory = await getExternalStorageDirectory();
+    setState(() {
+      videoDirectory = '${appDirectory.path}/Drupal_Videos/answers';
+      currentTime = DateTime.now().millisecondsSinceEpoch.toString();
+    });
+    await Directory(videoDirectory).create(recursive: true);
+
+    final String filePath = '$videoDirectory/$currentTime.mp4';
+    print('lsadkjfl;kalsfjdlaksdjfl;kajl;dfkjas');
+    print(filePath);
+    try {
+      await controller.startVideoRecording(filePath);
+      videoPath = filePath;
+    } on CameraException catch (e) {
+      _showCameraException(e);
+      return null;
+    }
+
+    return filePath;
+  }
+
+  void _showCameraException(CameraException e) {
+    String errorText = 'Error: ${e.code}\nError Message: ${e.description}';
+    print(errorText);
+
+    Fluttertoast.showToast(
+        msg: 'Error: ${e.code}\n${e.description}',
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIos: 1,
+        backgroundColor: Colors.red,
+        textColor: Colors.white);
+  }
+
+  void _onStopButtonPressed() async {
+    await _stopVideoRecording();
+
+  }
+
+   Future<void> _stopVideoRecording() async {
+    if (!controller.value.isRecordingVideo) {
+      return null;
+    }
+
+    try {
+      await controller.stopVideoRecording();
+      if(toUpload)
+        uploadAnswer(videoPath, widget.questionName);
+      else{
+        String newPath = videoDirectory + "/" + currentTime + "NotUploaded.mp4";
+        print(newPath);
+        File(videoPath).renameSync(newPath);
+      }
+    } on CameraException catch (e) {
+      _showCameraException(e);
+      return null;
+    }
+
+  }
+
+
+  uploadAnswer(String filePath, String questionName) async {
+  print("uploading");
+  // var url = "$serverIP:$serverPort/uploadAnswer";
+  var uri = new Uri.http('$serverIP:$serverPort', '/uploadAnswer');
+
+  var token = await getCurrentTokenId();
+
+  final String videoDirectory = '$filePath';
+
+  var request = new http.MultipartRequest("POST", uri);
+  print("successfuly parse the url $uri");
+
+  request.files.add( await http.MultipartFile.fromPath('video', videoDirectory, contentType: MediaType('video', 'mp4')));
+  request.fields['tokenId'] = token;
+  request.fields['questionName'] = questionName;
+
+  request.send().then((response) {
+    print(response.statusCode);
+    print(response.toString());
+    if (response.statusCode == 200){
+      print("Uploaded!");
+        Fluttertoast.showToast(
+        msg: 'Successfully uploaded',
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIos: 1,
+    );
+    }else{
+       Fluttertoast.showToast(
+        msg: 'Video failed to upload..',
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIos: 1,
+       );
+      String currentTime=  DateTime.now().millisecondsSinceEpoch.toString();
+      String newPath = videoDirectory.substring(0, videoDirectory.lastIndexOf("/")) + "/" + currentTime + "NotUploaded.mp4";
+        print(newPath);
+        File(filePath).renameSync(newPath);
+    }
+  });
+}
+}
